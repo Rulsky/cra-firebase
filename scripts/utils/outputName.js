ad3af48361b2d4d2c114c571b300c70b08f5673f
Ruslan Zaytsev <ruslan.zaytsev@gmail.com>
2018-01-08 06:38:38 +0200

    outputName with tests

--- scripts/utils/outputName.js
+++ scripts/utils/outputName.js
@@ -1,10 +1,10 @@
-const path = require('path')
+const { sep, join } = require('path')
 
 const { root } = require('../../config/filelist')
 
 const outputName = (filename, currDir, destDir) => {
-  const relName = filename.split(`${currDir}${path.sep}`)[1]
-  return path.join(root, destDir, relName)
+  const relName = filename.split(`${currDir}${sep}`)[1]
+  return join(root, destDir, relName)
 }
 
 module.exports = outputName
---
+++ spec/utils/outputName.spec.js
@@ -0,0 +1,29 @@
+const { join } = require('path')
+
+describe('producec correct absolute filename', () => {
+  let outputName
+  beforeEach(() => {
+    jest.mock('../../config/filelist')
+
+    // eslint-disable-next-line global-require
+    outputName = require('../../scripts/utils/outputName')
+  })
+
+  it('case 1', () => {
+    const currDir = join(process.cwd(), 'src')
+    const destDir = 'lib'
+    const inputName = join(currDir, 'testname.txt')
+    const expected = join(process.cwd(), destDir, 'testname.txt')
+
+    expect(outputName(inputName, currDir, destDir)).toEqual(expected)
+  })
+
+  it('case 2', () => {
+    const currDir = join(process.cwd(), 'test-test')
+    const destDir = 'functions'
+    const inputName = join(currDir, 'testname.txt')
+    const expected = join(process.cwd(), destDir, 'testname.txt')
+
+    expect(outputName(inputName, currDir, destDir)).toEqual(expected)
+  })
+})
