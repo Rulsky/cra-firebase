7073e5181e8edf69e2830f0a8a1ab023cff6ed21
Ruslan Zaytsev <ruslan.zaytsev@gmail.com>
2018-01-11 02:01:59 +0200

    improve init script with informing user about steps

--- scripts/init.js
+++ scripts/init.js
@@ -1,6 +1,13 @@
 const updatePack = require('./utils/updatePack.js')
 const updateGitignore = require('./utils/updateGitignore.js')
+const logging = require('./utils/logging')
 
-const init = (updP = updatePack, updG = updateGitignore) => Promise.all([updP(), updG()])
+const init = (updP = updatePack, updG = updateGitignore, l = logging) =>
+  Promise.resolve(l.infoOut('Modifying package.json scripts build command'))
+    .then(updP())
+    .then(l.infoOut('Success\nAdding new entries to .gitignore'))
+    .then(updG())
+    .then(l.okOut('Success\nFinish'))
+    .catch(err => l.errOut(`${err.stack}`))
 
 module.exports = init
--- spec/init.spec.js
+++ spec/init.spec.js
@@ -1,13 +1,42 @@
 describe('init script', () => {
   const updatePackMock = jest.fn(() => Promise.resolve())
   const updateGitignore = jest.fn(() => Promise.resolve())
-  it('runs updatePack and updateGitignore', () => {
+  const loggingMock = {
+    okOut: jest.fn(),
+    infoOut: jest.fn(),
+    errOut: jest.fn(),
+  }
+
+  beforeEach(() => {
+    jest.resetAllMocks()
+    jest.resetModules()
     jest.mock('../config/filelist')
+  })
+
+  it('runs updatePack and updateGitignore', () => {
     const init = require('../scripts/init') // eslint-disable-line global-require
     expect.assertions(2)
-    return init(updatePackMock, updateGitignore).then(() => {
+    return init(updatePackMock, updateGitignore, loggingMock).then(() => {
       expect(updatePackMock).toHaveBeenCalledTimes(1)
       expect(updateGitignore).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  it('informs about progress', () => {
+    const init = require('../scripts/init') // eslint-disable-line global-require
+    expect.assertions(2)
+    return init(updatePackMock, updateGitignore, loggingMock).then(() => {
+      expect(loggingMock.okOut).toHaveBeenCalledTimes(1)
+      expect(loggingMock.infoOut).toHaveBeenCalledTimes(2)
+    })
+  })
+
+  it('informs about failure', () => {
+    const init = require('../scripts/init') // eslint-disable-line global-require
+    expect.assertions(1)
+    const updatePackMockWithErr = jest.fn(() => () => Promise.reject(new Error()))
+    return init(updatePackMockWithErr, updateGitignore, loggingMock).then(() => {
+      expect(loggingMock.errOut).toHaveBeenCalledTimes(1)
     })
   })
 })
