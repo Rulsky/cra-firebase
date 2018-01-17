f6f5060e5de56d8e30b6194cf280555e292cd525
Ruslan Zaytsev <ruslan.zaytsev@gmail.com>
2018-01-17 06:03:27 +0200

    consume addRewrites.

--- scripts/init.js
+++ scripts/init.js
@@ -1,10 +1,13 @@
 const updatePack = require('./utils/updatePack.js')
 const updateGitignore = require('./utils/updateGitignore.js')
+const addRewrites = require('./utils/addRewrites')
 const logging = require('./utils/logging')
 
-const init = (updP = updatePack, updG = updateGitignore, l = logging) =>
+const init = (updP = updatePack, updG = updateGitignore, addR = addRewrites, l = logging) =>
   Promise.resolve(l.infoOut('Modifying package.json scripts build command'))
     .then(updP())
+    .then(l.infoOut('Success\nAdding rewrite rule to firebase.json'))
+    .then(addR())
     .then(l.infoOut('Success\nAdding new entries to .gitignore'))
     .then(updG())
     .then(l.okOut('Success\nFinish'))
