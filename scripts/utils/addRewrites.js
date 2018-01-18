a2a30c454b84c0fdb9826e82ce5066d35049f703
Ruslan Zaytsev <ruslan.zaytsev@gmail.com>
2018-01-18 11:56:24 +0200

    fix critical bug in addRewrites
    addRewrites wrongly composed final `fibase.json`.

--- scripts/utils/addRewrites.js
+++ scripts/utils/addRewrites.js
@@ -23,40 +23,58 @@ const addRewrites = () =>
     })
     .then(res => JSON.parse(res))
     .then((r) => {
-      if (r.rewrites && r.rewrites.length > 0) {
-        const idx = r.rewrites.findIndex(el => el.source === '**')
+      if (r.hosting.rewrites && r.hosting.rewrites.length > 0) {
+        const idx = r.hosting.rewrites.findIndex(el => el.source === '**')
         if (idx !== -1) {
-          if (r.rewrites[idx].destination && r.rewrites[idx].destination === '/index.html') {
+          if (
+            r.hosting.rewrites[idx].destination &&
+            r.hosting.rewrites[idx].destination === '/index.html'
+          ) {
             infoOut('Redefining "**" rewrite to point to app function')
             return {
               ...r,
-              rewrites: replaceElem(r.rewrites, idx, defaultRule),
+              hosting: {
+                ...r.hosting,
+                rewrites: replaceElem(r.hosting.rewrites, idx, defaultRule),
+              },
             }
-          } else if (r.rewrites[idx].function && r.rewrites[idx].function === 'app') {
+          } else if (
+            r.hosting.rewrites[idx].function &&
+            r.hosting.rewrites[idx].function === 'app'
+          ) {
             infoOut('A correct rewrite rule is already present. Skipping...')
             return null
-          } else if (r.rewrites[idx].function && r.rewrites[idx].function !== 'app') {
+          } else if (
+            r.hosting.rewrites[idx].function &&
+            r.hosting.rewrites[idx].function !== 'app'
+          ) {
             errOut(`In you firebase configuration '**' rewrite rule is alredy present and pointin to ${r
-              .rewrites[idx]
+              .hosting.rewrites[idx]
               .function} function.\nCan't redefine rewrite rule\nsource: '**',\nfunction: ${r
-              .rewrites[idx].function}\n\nPlease, resolve it manually.\nSkipping...`)
+              .hosting.rewrites[idx].function}\n\nPlease, resolve it manually.\nSkipping...`)
             return null
           }
           errOut(`In you firebase configuration '**' rewrite rule is alredy present and pointin to some custom file.\nCan't redefine rewrite rule\nsource: '**',\ndestination: ${r
-            .rewrites[idx].destination}\n\nPlease, resolve it manually.\nSkipping...`)
+            .hosting.rewrites[idx].destination}\n\nPlease, resolve it manually.\nSkipping...`)
           return null
         }
 
         infoOut('No rewrite ** rule found. Adding default rewrite rule.')
         return {
           ...r,
-          rewrites: [...r.rewrites, defaultRule],
+          hosting: {
+            ...r.hosting,
+            rewrites: [...r.hosting.rewrites, defaultRule],
+          },
         }
       }
       infoOut('No rewrite rules found. Adding default rule')
       return {
         ...r,
-        rewrites: [defaultRule],
+        hosting: {
+          ...r.hosting,
+          rewrites: [defaultRule],
+        },
       }
     })
     .then((res) => {
