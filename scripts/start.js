f0de511dbf861625895fd3bc30aee02894305a8f
Ruslan Zaytsev <ruslan.zaytsev@gmail.com>
2018-02-22 19:25:08 +0200

    start script working but not tested

--- scripts/start.js
+++ scripts/start.js
@@ -1,1 +1,51 @@
-console.log('start.js was called but it not implemented yet')
+const chokidar = require('chokidar')
+const chalk = require('chalk')
+const { sep } = require('path')
+const { removeSync } = require('fs-extra')
+
+const { input, firebaseFunctionsDir, srcDir } = require('../config/filelist')
+const outputName = require('./utils/outputName')
+const getFilterFiles = require('./utils/getFilterFiles')
+const transform = require('./utils/transform')
+const { errOut } = require('./utils/logging')
+
+const { info } = console
+const fileColor = str => info(chalk.cyan(str))
+const rmColor = str => info(chalk.magenta(str))
+
+const { include, exclude } = getFilterFiles()
+const watchList = [
+  input.index,
+  include.map(ext => `${input.shared}${sep}**${sep}*${ext}`),
+  include.map(ext => `${input.server}${sep}**${sep}*${ext}`),
+]
+
+const watcher = chokidar.watch(
+  watchList,
+  { ignored: exclude.map(el => `**/*${el}`) },
+)
+
+const start = (wt = watcher) => {
+  process.env.BABEL_ENV = 'development'
+  wt
+    .on('add', (path) => {
+      fileColor(`File ${path} has been added`)
+      transform(path)
+    })
+    .on('change', (path) => {
+      fileColor(`File ${path} has been changed`)
+      transform(path)
+    })
+    .on('unlink', (path) => {
+      removeSync(outputName(path, srcDir, firebaseFunctionsDir))
+      rmColor(`File ${path} has been removed`)
+    })
+
+  // More possible events.
+  wt
+    .on('error', error => errOut(`Watcher error: ${error}`))
+    .on('ready', () => info(chalk.green('First run is complete.\nListening for changes...')))
+
+  return wt
+}
+module.exports = start
